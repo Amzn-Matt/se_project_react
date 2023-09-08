26eee9f8838d22cb9eaa13b2c99616867aa8ae09
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-08 00:07:20 -0400

    refactor app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
-import logo from "../../logo.svg";
 import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
     <div>
-      <header></header>
+      <Header />
+      <Main />
     </div>
   );
 }
