b5dfb2225e3389791c96c2cabf427e25297187ec
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-08 20:11:46 -0400

    create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Developed by Matthew Sanchez</p>
+
+      <p>2022</p>
+    </footer>
+  );
+}
+
+export default Footer;
