84c897c033aed8123ed27306babd95f2d9957061
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-09 21:57:53 -0400

    add weather api component

---
+++ src/Utils/WeatherApi.js
@@ -0,0 +1,21 @@
+const latitude = 40.7128;
+const longitude = 74.006;
+const APIkey = "13f73a0fc9c7844102dd581da3e814e7";
+
+export const getWeatherApi = () => {
+  const WeatherApi = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  });
+  return WeatherApi;
+};
+
+export const parseWeatherData = (data) => {
+  const main = data.main;
+  const temperature = main && main.temp;
+  return Math.ceil(temperature);
+};
