ed91326944443f561dfbda5ce652c5c9804b9867
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-10 23:29:52 -0400

    refactor Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,29 @@
 import "./Header.css";
 
-function Header({ onOpenModal }) {
+function Header({ onOpenModal, userLocation }) {
   return (
-    <div>
-      <header className="header">
-        <div className="header__logo">
-          <div>
-            <img
-              src={require("../../images/Logo.svg").default}
-              alt="App logo"
-            />
-          </div>
-          <div>date</div>
-        </div>
+    <header className="header">
+      <div className="header__logo">
+        <img src={require("../../images/Logo.svg").default} alt="App logo" />
 
-        <div className="header__profile">
-          <div>
-            <button type="text" onClick={onOpenModal}>
-              + Add New Clothes
-            </button>
-          </div>
-          <div>Matthew Sanchez</div>
-          <div>
-            <img
-              src={require("../../images/Avatar.svg").default}
-              alt="Avatar icon"
-            />
-          </div>
+        <p className="header__date">September 10, {userLocation}</p>
+      </div>
+
+      <div className="header__profile">
+        <div>
+          <button className="header__add-btn" type="text" onClick={onOpenModal}>
+            + Add Clothes
+          </button>
         </div>
-      </header>
-    </div>
+        <div>Matthew Sanchez</div>
+        <div>
+          <img
+            src={require("../../images/Avatar.svg").default}
+            alt="Avatar icon"
+          />
+        </div>
+      </div>
+    </header>
   );
 }
 
