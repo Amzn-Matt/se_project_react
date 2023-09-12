7265c821693a2da402e318c13846c08657037083
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-12 19:16:29 -0400

    import weatherOptions from constants

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-const defaultClothingItems = [
+export const defaultClothingItems = [
   {
     _id: 0,
     name: "Cap",
@@ -37,4 +37,61 @@ const defaultClothingItems = [
   },
 ];
 
-export default defaultClothingItems;
+export const WeatherOptions = [
+  {
+    url: require("../images/Day/sunny.svg").default,
+    day: true,
+    type: "sunny",
+  },
+  {
+    url: require("../images/Day/cloudy.svg").default,
+    day: true,
+    type: "cloudy",
+  },
+  {
+    url: require("../images/Day/rainy.svg").default,
+    day: true,
+    type: "rainy",
+  },
+  {
+    url: require("../images/Day/snow.svg").default,
+    day: true,
+    type: "snow",
+  },
+  {
+    url: require("../images/Day/storm.svg").default,
+    day: true,
+    type: "storm",
+  },
+  { url: require("../images/Day/fog.svg").default, day: true, type: "fog" },
+  {
+    url: require("../images/Night/cloudy-night.svg").default,
+    day: false,
+    type: "cloudy",
+  },
+  {
+    url: require("../images/Night/night.svg").default,
+    day: false,
+    type: "night",
+  },
+  {
+    url: require("../images/Night/rain-night.svg").default,
+    day: false,
+    type: "rainy",
+  },
+  {
+    url: require("../images/Night/snow-night.svg").default,
+    day: false,
+    type: "snow",
+  },
+  {
+    url: require("../images/Night/storm-night.svg").default,
+    day: false,
+    type: "storm",
+  },
+  {
+    url: require("../images/Night/fog-night.svg").default,
+    day: false,
+    type: "fog",
+  },
+];
