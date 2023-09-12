05b2c00ddf5f0466a64a372c5f1fc9bd73d17e5d
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-12 19:15:35 -0400

    move submit btn into the form

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -10,16 +10,14 @@ function ModalWithForm({
   return (
     <div className={`modal modal_type_${name}`}>
       <div className="modal__container">
-        <button
-          className="modal__close"
-          type="button"
-          onClick={onCloseModal}
-        ></button>
+        <button className="modal__close" type="button" onClick={onCloseModal} />
         <h3 className="modal__form-title">{title}</h3>
-        <form className="modal__form">{children}</form>
-        <button className="modal__submit-btn" type="submit">
-          {buttonText}
-        </button>
+        <form className="modal__form">
+          {children}
+          <button className="modal__submit-btn" type="submit">
+            {buttonText}
+          </button>
+        </form>
       </div>
     </div>
   );
