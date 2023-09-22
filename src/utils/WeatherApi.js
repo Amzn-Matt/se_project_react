310ca5491753de90f9a5818cd6663a816afefd8c
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-21 23:17:02 -0400

    refactor weather  api call to display temp unit

--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -17,7 +17,14 @@ export const getWeatherApi = () => {
 export const parseWeatherData = (data) => {
   const main = data.main;
   const temperature = main && main.temp;
-  return Math.ceil(temperature);
+  const weather = {
+    temperature: {
+      F: Math.round(temperature),
+      C: Math.round(((temperature - 32) * 5) / 9),
+    },
+  };
+  console.log(weather);
+  return weather;
 };
 
 export const parseLocationData = (data) => {
