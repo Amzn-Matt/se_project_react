af3ef0f8ee2e123f5c28ea0ba6964612e344a49f
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-23 15:05:09 -0400

    create api component

---
+++ src/utils/api.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const baseUrl = "http://localhost:3001";
+
+export const getClothingItems = () => {
+  return fetch(`${baseUrl}/items`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  });
+};
+
+export const addNewClothingItem = (item) => {
+  return fetch(`${baseUrl}/items`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: item.name,
+      imageUrl: item.url,
+      weather: item.weatherType,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  });
+};
