790bf4fa94de2f15cf01b09edc9a32a0bad6559d
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-22 23:55:01 -0400

    refactor ModalWithForm props

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -6,13 +6,15 @@ function ModalWithForm({
   title,
   name,
   onCloseModal,
+  isOpen,
+  onSubmit,
 }) {
   return (
     <div className={`modal modal_type_${name}`}>
       <div className="modal__container">
         <button className="modal__close" type="button" onClick={onCloseModal} />
         <h3 className="modal__form-title">{title}</h3>
-        <form className="modal__form">
+        <form className="modal__form" onSubmit={onSubmit}>
           {children}
           <button className="modal__submit-btn" type="submit">
             {buttonText}
