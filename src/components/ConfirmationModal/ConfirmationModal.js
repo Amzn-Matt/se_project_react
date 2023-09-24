81ce72b369ec9db58a22e0feb85a389594208980
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-09-24 18:02:39 -0400

    create ConfirmationModal component

---
+++ src/components/ConfirmationModal/ConfirmationModal.js
@@ -0,0 +1,40 @@
+import "./ConfirmationModal.css";
+
+const ConfirmationModal = ({ selectedCard, onCloseModal, onDeleteItem }) => {
+  const handleDeleteItemSubmit = () => {
+    onDeleteItem(selectedCard._id);
+  };
+  return (
+    <div className={`modal`}>
+      <div className="modal__confirm-container">
+        <button
+          className="modal__confrim-close-btn"
+          type="button"
+          onClick={onCloseModal}
+        />
+        <div className="modal__confirm-text-wrapper">
+          <p className="modal__confirm-text">
+            Are you sure you want to delete this item? This action is
+            irreversable.
+          </p>
+        </div>
+        <button
+          className="modal__confirm-delete-btn"
+          type="button"
+          onClick={handleDeleteItemSubmit}
+        >
+          Yes, delete item
+        </button>
+        <button
+          className="modal__confirm-cancel-btn"
+          type="button"
+          onClick={onCloseModal}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
