a6f00f1c91f952b4d25b948758a6c50c87c1eee4
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-11-01 21:22:39 -0400

    create auth api requests

---
+++ src/utils/auth.js
@@ -0,0 +1,23 @@
+import { request } from "./api";
+
+const baseUrl = "http://localhost:3001";
+
+export const signup = ({ name, avatar, email, password }) => {
+  return request(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  });
+};
+
+export const signin = ({ email, password }) => {
+  return request(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+};
