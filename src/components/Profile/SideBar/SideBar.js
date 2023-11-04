710967b927e280d9ac9ae9ba05245fa4461c6490
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-11-04 15:13:50 -0400

    refactor sidebar

--- src/components/Profile/SideBar/SideBar.js
+++ src/components/Profile/SideBar/SideBar.js
@@ -2,24 +2,31 @@ import "./SideBar.css";
 import { CurrentUserContext } from "../../../contexts/CurrentUserContext";
 import { useContext } from "react";
 
-const SideBar = ({ onChangeProfile, onLogout }) => {
+const SideBar = ({ onEditProfile, onLogout }) => {
   const currentUser = useContext(CurrentUserContext);
+  const currentAvatar = currentUser.avatar !== "" ? true : false;
 
   return (
     <div className="sidebar">
-      <div>
-        <img
-          className="sidebar__avatar"
-          src={currentUser.avatar}
-          alt="Avatar icon"
-        />
+      <div className="sidebar__profile-info">
+        {currentAvatar ? (
+          <img
+            className="sidebar__avatar"
+            src={currentUser.avatar}
+            alt="Avatar icon"
+          />
+        ) : (
+          <p className="sidebar__avatar-default">
+            {currentUser.name[0].toUpperCase()}
+          </p>
+        )}
+        <p className="sidebar__name">{currentUser.name}</p>
       </div>
-      <p className="sidebar__name">{currentUser.name}</p>
-      <div>
+      <div className="sidebar__profile-manager">
         <button
           className="sidebar__edit-btn"
           type="button"
-          onClick={onChangeProfile}
+          onClick={onEditProfile}
         >
           Change Profile Data
         </button>
