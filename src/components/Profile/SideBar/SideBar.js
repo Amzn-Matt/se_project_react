d36987bb16b0627ee7726d44a529ac06b20c67bb
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-11-04 10:42:37 -0400

    refactor sidebar

--- src/components/Profile/SideBar/SideBar.js
+++ src/components/Profile/SideBar/SideBar.js
@@ -1,13 +1,36 @@
 import "./SideBar.css";
-import avatar from "../../../images/Avatar.svg";
+import { CurrentUserContext } from "../../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-const SideBar = () => {
+const SideBar = ({ onChangeProfile, onLogout }) => {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <div className="sidebar">
       <div>
-        <img className="sidebar__avatar" src={avatar} alt="Avatar icon" />
+        <img
+          className="sidebar__avatar"
+          src={currentUser.avatar}
+          alt="Avatar icon"
+        />
       </div>
-      <p className="sidebar__name">Matthew Sanchez</p>
+      <p className="sidebar__name">{currentUser.name}</p>
+      <div>
+        <button
+          className="sidebar__edit-btn"
+          type="button"
+          onClick={onChangeProfile}
+        >
+          Change Profile Data
+        </button>
+        <button
+          className="sidebar__logout-btn"
+          type="button"
+          onClick={onLogout}
+        >
+          Log out
+        </button>
+      </div>
     </div>
   );
 };
